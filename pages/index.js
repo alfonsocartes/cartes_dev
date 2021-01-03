12d363d8e518d09e6eb8fd121f0f980c43006e1a
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-01-03 14:06:58 +0000

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Head from "next/head";
+import Container from "../components/container";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import MoreStories from "../components/more-stories";
+import { getAllPosts } from "../lib/api";
+import { CMS_NAME } from "../lib/constants";
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+
   return (
     <>
       <Layout>
@@ -32,20 +33,20 @@ export default function Index({ allPosts }) {
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
   const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
 
   return {
     props: { allPosts },
-  }
+  };
 }

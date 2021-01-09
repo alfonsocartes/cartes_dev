d80667560316103f7a8ca71e895213356469167b
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-01-09 15:38:27 +0000

    Change footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,29 +1,158 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
   return (
-    <div className="bg-gray-200 dark:bg-gray-800">
-      <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-        <a
-          href="https://www.cartes.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 dark:text-gray-200"
-        >
-          <span className="ml-3 text-xl">cartes.dev</span>
-        </a>
-        <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-          © {new Date().getFullYear()} Alfonso Cartes —
-          <a
-            href="https://twitter.com/cartes_dev"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-600 dark:text-gray-300 ml-1"
-          >
-            @cartes_dev
-          </a>
-        </p>
+    <footer className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+        <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+          <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <span className="ml-3 text-xl">cartes.dev</span>
+          </div>
+          <p className="mt-2 text-sm text-gray-500">Software Development</p>
+          <p className="mt-2 text-sm text-gray-500">
+            Full-Stack Web Development
+          </p>
+          <p className="mt-2 text-sm text-gray-500">Mobile Development</p>
+        </div>
+        <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+          <div className="lg:w-1/2 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              CATEGORIES
+            </h2>
+            <nav className="list-none mb-10">
+              <li>
+                <div className="text-gray-600 hover:text-gray-800">
+                  <Link href="/">First Link</Link>
+                </div>
+              </li>
+              <li>
+                <div className="text-gray-600 hover:text-gray-800">
+                  <Link href="/">Second Link</Link>
+                </div>
+              </li>
+              <li>
+                <div className="text-gray-600 hover:text-gray-800">
+                  <Link href="/">Third Link</Link>
+                </div>
+              </li>
+              <li>
+                <div className="text-gray-600 hover:text-gray-800">
+                  <Link href="/">Forth Link</Link>
+                </div>
+              </li>
+            </nav>
+          </div>
+          <div className="lg:w-1/2 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              CATEGORIES
+            </h2>
+            <nav className="list-none mb-10">
+              <li>
+                <div className="text-gray-600 hover:text-gray-800">
+                  <Link href="/">First Link</Link>
+                </div>
+              </li>
+              <li>
+                <div className="text-gray-600 hover:text-gray-800">
+                  <Link href="/">Second Link</Link>
+                </div>
+              </li>
+              <li>
+                <div className="text-gray-600 hover:text-gray-800">
+                  <Link href="/">Third Link</Link>
+                </div>
+              </li>
+              <li>
+                <div className="text-gray-600 hover:text-gray-800">
+                  <Link href="/">Forth Link</Link>
+                </div>
+              </li>
+            </nav>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="bg-gray-100">
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
+            © {new Date().getFullYear()} Alfonso Cartes —
+            <a
+              href="https://twitter.com/cartes_dev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @cartes_dev
+            </a>
+          </p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <div className="pl-4 text-gray-500">
+              <a
+                href="https://twitter.com/cartes_dev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </div>
+              </a>
+            </div>
+            <div className="pl-4 text-gray-500">
+              <a
+                href="https://www.linkedin.com/company/cartesdev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>
+                  <svg
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="0"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="none"
+                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                    ></path>
+                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                  </svg>
+                </div>
+              </a>
+            </div>
+            <div className="pl-4 text-gray-500">
+              <Link
+                href="https://www.facebook.com/cartes.dev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </div>
+              </Link>
+            </div>
+          </span>
+        </div>
+      </div>
+    </footer>
   );
 }

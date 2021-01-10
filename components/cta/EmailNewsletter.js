c413f338a532489067bbbb7d0278c6da0915b459
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-01-10 09:20:54 +0000

    Fix CSS Darkmode and frosted buttons

--- components/cta/EmailNewsletter.js
+++ components/cta/EmailNewsletter.js
@@ -34,7 +34,7 @@ const EmailNewsletter = () => (
             className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:gray-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           />
         </div>
-        <button className="text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">
+        <button className="frosted bg-opacity-40 border-white border-opacity-20 border rounded-lg shadow-lg bg-gray-300  hover:bg-gray-300 dark:hover:bg-gray-600  py-2 px-8 focus:outline-none text-lg">
           Subscribe
         </button>
       </div>

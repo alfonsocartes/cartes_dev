6a23683b69d0c5e957392a52c201030f49fa7555
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-01-18 17:28:59 +0000

    Add Teck Stack component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Layout from "../components/layout/Layout";
 import HeroPersonal from "../components/HeroPersonal";
 import EmailNewsletter from "../components/cta/EmailNewsletter";
+import TechStack from "../components/TechStack";
 
 export default function Index() {
   return (
     <>
       <Layout title="Cartes Development | Full-Stack Web Development / Mobile Development">
         <HeroPersonal />
+        <TechStack />
         <EmailNewsletter />
       </Layout>
     </>

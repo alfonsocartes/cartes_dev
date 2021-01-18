735792e23d7c79bd9df5643a75aad3ce4cc7b0ce
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-01-18 16:07:47 +0000

    Fix Header padding responsive

--- components/layout/header.js
+++ components/layout/header.js
@@ -6,8 +6,8 @@ import NavBar from "./NavBar";
 export default function Header() {
   return (
     <header className="frosted bg-opacity-40 border-white border-opacity-20 border-b shadow-lg bg-gray-200 dark:bg-transparent sticky top-0 z-10 mb-4">
-      <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
-        <div className="text-3xl p-10 font-bold">
+      <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center p-8 pb-4">
+        <div className="text-3xl font-bold pb-4">
           <Link href="/">
             <Logo />
           </Link>

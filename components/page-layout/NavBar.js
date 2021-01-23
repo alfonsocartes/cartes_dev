32ba0d2a2d7795edcb187acca4853e3bc238fb17
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-01-23 21:48:16 +0000

    Fix click area target

--- components/page-layout/NavBar.js
+++ components/page-layout/NavBar.js
@@ -4,21 +4,36 @@ import DarkModeToggle from "./DarkModeToggle";
 
 const NavBar = () => (
   <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-    <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
-      <Link href="/">Home</Link>
-    </div>
-    <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
-      <Link href="portfolio">Portfolio</Link>
-    </div>
-    {/* <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
-      <Link href="/">Blog</Link>
-    </div> */}
-    <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
-      <Link href="about">About</Link>
-    </div>
-    <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
-      <Link href="contact">Contact</Link>
-    </div>
+    <Link href="/">
+      <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
+        Home
+      </div>
+    </Link>
+    {/* <Link href="solutions">
+      <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
+        Solutions
+      </div>
+    </Link> */}
+    <Link href="portfolio">
+      <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
+        Portfolio
+      </div>
+    </Link>
+    <Link href="about">
+      <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
+        About
+      </div>
+    </Link>
+    <Link href="contact">
+      <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
+        Contact
+      </div>
+    </Link>
+    {/* <Link href="blog">
+      <div className="rounded-lg py-2 px-3 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600">
+        Blog
+      </div>
+    </Link> */}
     <DarkModeToggle />
   </nav>
 );

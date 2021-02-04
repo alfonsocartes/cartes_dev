4a3ceb4152a64ba974c43479646aa13c5d3a0add
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-02-04 21:40:06 +0000

    Add contact form with recaptcha

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,12 @@ import Head from "next/head";
 import Footer from "./page-layout/Footer";
 import Header from "./page-layout/Header";
 
-export default function Layout({ title, children }) {
+export default function Layout({ title, optionalScript, children }) {
   return (
-    <div className="bg-white dark:bg-gray-900 text-black dark:text-gray-200 flex flex-col min-h-screen ">
+    <div className="bg-white dark:bg-gray-900 text-black dark:text-gray-200 flex flex-col min-h-screen">
       <Head>
         <title>{title}</title>
+        {optionalScript}
       </Head>
       <Header />
       <main className="container mx-auto px-5 py-10 flex-grow">{children}</main>

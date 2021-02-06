29a21c0c03cf11687a6d82a03a8dc62fc77dd6dc
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-02-06 10:01:54 +0000

    Add blog post list page

---
+++ components/blog/PostListItem.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import DateFormatter from "./DateFormatter";
+
+const PostListItem = ({ post }) => {
+  return (
+    <section>
+      <h4 className="text-2xl font-medium title-font mb-2  text-indigo-600 hover:text-indigo-400 dark:text-indigo-400 dark:hover:text-indigo-200">
+        <Link href={`/posts/${post.slug}`}>
+          <a>{post.title}</a>
+        </Link>
+      </h4>
+
+      <p className="text-sm text-gray-500">
+        <DateFormatter dateString={post.date} />
+      </p>
+      <p className="text-lg">{post.excerpt}</p>
+    </section>
+  );
+};
+
+export default PostListItem;

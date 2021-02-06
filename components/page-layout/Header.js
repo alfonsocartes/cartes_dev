fefa538a44882290817c2866e5e74e92f17e192c
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-02-06 11:27:41 +0000

    Remove header background color

--- components/page-layout/Header.js
+++ components/page-layout/Header.js
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 
 export default function Header() {
   return (
-    <header className="frosted bg-opacity-40 border-white border-opacity-20 border-b shadow-lg bg-gray-200 sticky top-0 z-10 mb-4">
+    <header className="frosted bg-opacity-40 border-white border-opacity-20 border-b shadow-lg sticky top-0 z-10 mb-4">
       <div className="container mx-auto flex flex-wrap flex-col sm:flex-row items-center pl-4 py-4">
         <div className="text-3xl dark:text-white font-bold mb-4 md:mb-0">
           <Logo />

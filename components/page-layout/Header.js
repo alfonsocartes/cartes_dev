98f229615e146ca67b3537e374edc27ceabc08b4
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-03-06 10:35:22 +0000

    Add background color to header for light mode

--- components/page-layout/Header.js
+++ components/page-layout/Header.js
@@ -1,9 +1,8 @@
-import Logo from "../Logo";
 import NavBar from "./NavBar";
 
 export default function Header() {
   return (
-    <header className="frosted bg-opacity-40 border-white border-opacity-20 border-b shadow-lg sticky top-0 z-10 items-center sm:p-8 p-4">
+    <header className="frosted bg-white dark:bg-transparent bg-opacity-40 border-white border-opacity-20 border-b shadow-lg sticky top-0 z-10 items-center sm:p-8 p-4">
       <NavBar />
     </header>
   );

78aec62415b26816d5cfdd9330b4cba3a08126ee
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-03-07 10:21:41 +0000

    Change sort post by dates

--- pages/portfolio/mobile-development/index.js
+++ pages/portfolio/mobile-development/index.js
@@ -2,15 +2,20 @@ import Layout from "../../../components/Layout";
 import ProjectList from "../../../components/projects/ProjectList";
 import { getAllFilesFrontMatter } from "../../../lib/mdx";
 
-const MobilePortfolio = ({ allPosts }) => (
-  <Layout title="Mobile Portfolio | Cartes Development">
-    <ProjectList
-      title="Mobile Development Portfolio"
-      path="/portfolio/mobile-development/mobile-projects"
-      allPosts={allPosts}
-    />
-  </Layout>
-);
+const MobilePortfolio = ({ allPosts }) => {
+  const sortedPosts = allPosts.sort(
+    (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+  );
+  return (
+    <Layout title="Mobile Portfolio | Cartes Development">
+      <ProjectList
+        title="Mobile Development Portfolio"
+        path="/portfolio/mobile-development/mobile-projects"
+        allPosts={sortedPosts}
+      />
+    </Layout>
+  );
+};
 
 export async function getStaticProps() {
   const allPosts = await getAllFilesFrontMatter("mobile-projects");
--- pages/portfolio/web-development/index.js
+++ pages/portfolio/web-development/index.js
@@ -2,15 +2,20 @@ import Layout from "../../../components/Layout";
 import ProjectList from "../../../components/projects/ProjectList";
 import { getAllFilesFrontMatter } from "../../../lib/mdx";
 
-const WebPortfolio = ({ allPosts }) => (
-  <Layout title="Web Portfolio | Cartes Development">
-    <ProjectList
-      title="Web Development Portfolio"
-      path="/portfolio/web-development/web-projects"
-      allPosts={allPosts}
-    />
-  </Layout>
-);
+const WebPortfolio = ({ allPosts }) => {
+  const sortedPosts = allPosts.sort(
+    (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+  );
+  return (
+    <Layout title="Web Portfolio | Cartes Development">
+      <ProjectList
+        title="Web Development Portfolio"
+        path="/portfolio/web-development/web-projects"
+        allPosts={sortedPosts}
+      />
+    </Layout>
+  );
+};
 
 export async function getStaticProps() {
   const allPosts = await getAllFilesFrontMatter("web-projects");

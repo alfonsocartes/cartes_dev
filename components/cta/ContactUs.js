24239642506e7538f7d205c7b72100fa308c8450
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2021-03-28 16:34:15 +0100

    Accesibility change tag importance for screen readers

--- components/cta/ContactUs.js
+++ components/cta/ContactUs.js
@@ -4,9 +4,9 @@ const ContactUs = () => (
   <section className="body-font">
     <div className="container mt-10 mx-auto">
       <div className="lg:w-2/3 flex flex-col sm:flex-row items-center mx-auto w-full pt-8 mt-8 border-t border-gray-200 text-center">
-        <h4 className="flex-grow sm:pr-16 sm:text-2xl text-lg font-medium title-font">
+        <h6 className="flex-grow sm:pr-16 sm:text-2xl text-lg font-medium title-font">
           Tell me about your project.
-        </h4>
+        </h6>
         <Link href="/contact">
           <button
             type="button"

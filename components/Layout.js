8029ed5efd57757ddce1f03d69653cf9f4976ebe
Alfonso Cartes <3504951+alfonsocartes@users.noreply.github.com>
2022-03-26 16:54:42 +0100

    feat: add google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -40,6 +40,23 @@ export default function Layout({ children, ...customMeta }) {
         {meta.date && (
           <meta property="article:published_time" content={meta.date} />
         )}
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <Header />
       <main className="container mx-auto px-5 py-10 flex-grow">{children}</main>

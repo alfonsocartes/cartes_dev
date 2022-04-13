29332a2d719fa9b693ab76ce4947eb4c9c7a2ec0
Alfonso Cartes <3504951+alfonsocartes@users.noreply.github.com>
2022-04-13 13:59:54 +0200

    feat: add carousel component

---
+++ components/Carousel.js
@@ -0,0 +1,9 @@
+export const Carousel = ({ images }) => (
+  <ul className="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw]">
+    {images.map((image, index) => (
+      <li key={index} className="shrink-0 snap-center">
+        <img src={image.src} alt={image.alt} />
+      </li>
+    ))}
+  </ul>
+);

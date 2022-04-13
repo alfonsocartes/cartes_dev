f005b6590866b103478acc66f65cc0deb9fb2041
Alfonso Cartes <3504951+alfonsocartes@users.noreply.github.com>
2022-04-13 19:33:32 +0200

    chore: carousel component notice

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,3 +1,5 @@
+// WIP: THIS IS A WORK IN PROGRESS
+
 export const Carousel = ({ images }) => (
   <ul className="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw]">
     {images.map((image, index) => (

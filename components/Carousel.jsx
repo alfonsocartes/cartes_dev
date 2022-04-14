0309546fa817bce0417f6b49bf685d180a758f1b
Alfonso Cartes <3504951+alfonsocartes@users.noreply.github.com>
2022-04-14 10:57:34 +0200

    feat: carousel css

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -30,18 +30,19 @@ export const Carousel = ({ post }) => {
   }
 
   return (
-    <ul className="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[10vw] after:shrink-0 after:w-[20vw]">
+    <ul className="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw] my-4">
       {images.map((image, index) => (
-        <li key={index} className="shrink-0 snap-center">
-          <div className="h-48">
-            <Image
-              src={image.url}
-              width={image.width}
-              height={image.height}
-              alt={image.alt}
-              layout=""
-            />
-          </div>
+        <li
+          key={index}
+          className="shrink-0 snap-center w-5/6 marker:text-transparent my-4"
+        >
+          <Image
+            src={image.url}
+            width={image.width}
+            height={image.height}
+            alt={image.alt}
+            layout="responsive"
+          />
         </li>
       ))}
     </ul>

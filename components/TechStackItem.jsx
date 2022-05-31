013a48754881ae6bbf5f05addf394360d3ee3fcf
Alfonso Cartes <3504951+alfonsocartes@users.noreply.github.com>
2022-05-31 23:17:32 +0200

    feat: remove style from tech stack item logos

--- components/TechStackItem.jsx
+++ components/TechStackItem.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { LazyImage } from "./LazyImage";
 
 export const TechStackItem = ({ title, imageUrl, url, children }) => {
@@ -11,8 +12,8 @@ export const TechStackItem = ({ title, imageUrl, url, children }) => {
   return (
     <article className="my-4 border-gray-200 p-4 md:rounded-lg md:border">
       <div className="grid grid-flow-row items-center md:grid-flow-col ">
-        <figure className="m-4 h-40 w-40 items-center justify-self-center overflow-hidden rounded-xl p-4 text-center dark:bg-gray-500">
-          <LazyImage image={image} />
+        <figure className="m-4 h-40 w-40 items-center justify-self-center overflow-hidden rounded-xl bg-slate-100 p-4 text-center shadow-sm dark:bg-slate-600">
+          <LazyImage image={image} noStyle />
         </figure>
         <div>
           <h2 className="title-font mb-2 text-center text-lg font-medium text-indigo-600  hover:text-indigo-400 dark:text-indigo-400 dark:hover:text-indigo-600 md:text-left">
